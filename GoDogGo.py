74b5880bb6f17c8d5b3d2bbda883dd0e61af65bf
bumb11ebee <74787961+bumb11ebee@users.noreply.github.com>
2020-11-20 12:48:44 -0700

    Will only read on OSX

--- GoDogGo.py
+++ GoDogGo.py
@@ -1,4 +1,4 @@
-import os, time
+import os, time, platform
 
 def say(voice,message):
     os.system('say -v '+voice+' '+message)
@@ -9,11 +9,14 @@ def say(voice,message):
 if __name__ == '__main__':
     v1 = 'Alice'
     v2 = 'Alex'
-    f = open("Book.txt", "r")
-    whichvoice = True
-    for x in f:
-        tmp = x.split('|')
-        if tmp[0] == 'v1':
-            say(v1,tmp[1])
-        elif tmp[0] == 'v2':
-            say(v2,tmp[1])
+    if platform.system() == 'Darwin':
+        f = open("Book.txt", "r")
+        whichvoice = True
+        for x in f:
+            tmp = x.split('|')
+            if tmp[0] == 'v1':
+                say(v1,tmp[1])
+            elif tmp[0] == 'v2':
+                say(v2,tmp[1])
+    else:
+        print('Sorry this was built for a Mac')
