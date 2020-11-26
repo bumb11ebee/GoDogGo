aa93dbf6cc5b32dbab60ad785c0d7cb9ab504fd4
bumb11ebee <74787961+bumb11ebee@users.noreply.github.com>
2020-11-25 18:10:50 -0700

    Commented my code

--- GoDogGo.py
+++ GoDogGo.py
@@ -10,13 +10,14 @@ if __name__ == '__main__':
     v1 = 'Alice'
     v2 = 'Alex'
     if platform.system() == 'Darwin':
-        f = open("Book.txt", "r")
-        whichvoice = True
-        for x in f:
-            tmp = x.split('|')
-            if tmp[0] == 'v1':
+        #If this is a Macbook then open the file
+        f = open("Book.txt", "r") #reads the words in the book
+
+        for each_line in f:
+            tmp = each_line.split('|')#Splits the line into a voice and a sentence
+            if tmp[0] == 'v1': #if the voice is v1 then read the line as v1
                 say(v1,tmp[1])
-            elif tmp[0] == 'v2':
+            elif tmp[0] == 'v2': #if the voice is v2 then read the line as v2
                 say(v2,tmp[1])
     else:
         print('Sorry this was built for a Mac')
